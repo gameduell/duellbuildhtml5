5288f2e577b6ddb95b394650406820d7afbbd43d
kgar <khaled.garbaya@gamduell.de>
2014-09-02 17:21:37 +0200

    update log format and viewport size for slimerjs

--- bin/test.js
+++ bin/test.js
@@ -2,7 +2,7 @@ var page = require('webpage').create();
 page.open('http://localhost:3000', function() 
 {
 });
-
+page.viewportSize = { width:1024, height:768 };
 page.onConsoleMessage = function(message, line, file){
-    console.log("[DEBUG] "+file+" : "+line+" : "+message);
+    console.log("[DEBUG] "+file+":"+line+": "+message);
 }
