f9d749a68879f4e9cae228d4a0bf2f32237e0955
Fabian Wulf <fabian.wulf@gameduell.de>
2015-07-06 18:08:02 +0200

    updated source documentation

--- duell/build/plugin/platform/PlatformBuild.hx
+++ duell/build/plugin/platform/PlatformBuild.hx
@@ -415,7 +415,12 @@ class PlatformBuild
 		/// RUN THE LISTENER
 		try
 		{
-			// TODO: Find a better/central place for the hardcoded fallback port 8181 ?
+			/**
+            * TODO: Find a better/central place for the hardcoded fallback port 8181
+            *       which is intended fall back on if the duell-tool's configuration
+            *       does not provide the TEST_PORT property (backward-compatibility).
+            *       Remove eventually...
+            **/
 			var testPort:Int = untyped Configuration.getData().TEST_PORT == null ?
 				8181 : Configuration.getData().TEST_PORT;
 
